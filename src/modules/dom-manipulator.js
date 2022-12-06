7659a2fdbc42308f71abd9030f0793144e8aa270
Lucas Santos <lucas.silva1203@outlook.com>
2022-12-06 13:19:30 -0300

    Add dom manipulator for the app
    Dom manipulator functions are now in a different script.

---
+++ src/modules/dom-manipulator.js
@@ -0,0 +1,63 @@
+let weatherAppDomManipulator = (function () {
+	let createDivElement = () => document.createElement("div");
+	let createBtnElement = () => document.createElement("button");
+	let createInptElement = () => document.createElement(`input`);
+    let createStrongElement = () => document.createElement("strong")
+
+	let createSearchContainer = function () {
+		let mainContainer = createDivElement();
+		mainContainer.id = "clock-search-container";
+
+		let searchContainer = createDivElement();
+		searchContainer.id = "search-container";
+
+		let searchInpt = createInptElement();
+		searchInpt.placeholder = "Type a city here...";
+
+		let searchBtn = createBtnElement();
+		searchBtn.textContent = "Search";
+		searchContainer.append(searchInpt, searchBtn);
+
+        //
+        let clockContainer = createDivElement()
+        let hours = createStrongElement()
+        hours.id = "weather-clock"
+
+
+        let timeout = setInterval(updateClock, 1000);
+
+        clockContainer.append(hours)
+
+    	mainContainer.append(searchContainer, clockContainer);
+		return mainContainer;
+	};
+
+	let createWeatherCardContainer = function () {
+		let container = createDivElement();
+
+		return container;
+	};
+
+	return { createSearchContainer };
+})();
+
+let updateClock = function(){
+    let clock = document.querySelector("#weather-clock")
+    let time = new Date()
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+
+    clock.textContent = `${hours}:${minutes}`
+}
+
+let createWeatherPanel = function () {
+	let screen = document.querySelector("#main-container");
+	screen.textContent = "";
+
+	let searchContainer = weatherAppDomManipulator.createSearchContainer();
+	//let weatherCardContainer = createWeatherCardContainer();
+
+	screen.append(searchContainer);
+};
+
+export default createWeatherPanel;
