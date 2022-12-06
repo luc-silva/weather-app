9c1b109a62e69446f9e27d3ab7352fb8740bae7f
Lucas Santos <lucas.silva1203@outlook.com>
2022-12-06 19:06:58 -0300

    Move getData from index.js to fetch.js
    The intention was to reutilize getData in other scritps. It was not
    possible before due to the index.js being the main source of the whole
    project

---
+++ src/modules/fetch.js
@@ -0,0 +1,13 @@
+async function getData(city = "London", unit = "metric") {
+	try {
+		let response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=67556afc5b803ebc6540b49ddb13c6a3&units=${unit}`
+		);
+		let data = await response.json();
+		console.log(data);
+		return data;
+	} catch (error) {
+		throw new Error(`Wasn't possible to fetch data from API. ${error}`);
+	}
+}
+export default getData;
