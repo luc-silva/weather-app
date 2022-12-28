6a3c397f2667d509e066f2177a9fde774bf7b65b
Lucas Santos <lucas.silva1203@outlook.com>
2022-12-28 13:58:00 -0300

    Add MainBody React Component

---
+++ src/components/MainBody.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class MainBody extends Component {
+    constructor(props: object) {
+        super(props);
+    }
+    render(){
+        return (
+            <div id="main-container">
+                
+            </div>
+        )
+    }
+}
+
+export default MainBody;
